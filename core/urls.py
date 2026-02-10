739e4ef388c318dd896a00d4d5e028a5b8fc49e5
Jakub Paluch <qbusik@gmail.com>
2026-02-10 06:31:31 +0100

    add simple serializers and views for core models

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from core.views import PostViewSet, CommentViewSet, ProfileViewSet
+
 app_name = "core"
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register('posts', PostViewSet)
+router.register('comments', CommentViewSet)
+router.register("profiles", ProfileViewSet)
+
+urlpatterns = [path("", include(router.urls))]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from core.models import Profile, Post, Comment
+from core.serializers import ProfileSerializer, PostSerializer, CommentSerializer
+
+
+class StandardPagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
