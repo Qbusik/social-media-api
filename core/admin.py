a25a1f4873afced536de6e6294cff10f25df3db7
Jakub Paluch <qbusik@gmail.com>
2026-02-10 06:11:32 +0100

    add core models and register in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Profile, Post, Comment
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,74 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.text import slugify
+
+
+def profile_image_file_path(
+    instance,
+    filename,
+):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.user.email)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/profile/", filename)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=150, blank=True)
+    country = models.CharField(max_length=150, blank=True)
+    bio = models.TextField(blank=True)
+    picture = models.ImageField(
+        blank=True, null=True, upload_to=profile_image_file_path
+    )
+    followers = models.ManyToManyField(
+        get_user_model(), related_name="following", blank=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+
+def post_image_file_path(
+    instance,
+    filename,
+):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.user.email)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/posts/", filename)
+
+
+class Post(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+    picture = models.ImageField(blank=True, null=True, upload_to=post_image_file_path)
+    likes = models.ManyToManyField(
+        get_user_model(), related_name="liked_posts", blank=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Post by: {self.user.email}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by: {self.user.email}"
