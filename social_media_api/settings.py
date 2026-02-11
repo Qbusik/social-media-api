0c5e49a9624e4b32813e25a323df24c93105e8b3
Jakub Paluch <qbusik@gmail.com>
2026-02-11 07:19:11 +0100

    add celery and redis

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -126,3 +126,9 @@ SPECTACULAR_SETTINGS = {
         "defaultModelExpandDepth": 2,
     },
 }
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_TIMEZONE = "Europe/Warsaw"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
